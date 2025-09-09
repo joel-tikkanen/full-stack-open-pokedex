c2ab41990c0817316dcdc7d794fd549fb1f6786b
joeltikkanen <joel.juhani.tikkanen@gmail.com>
2025-09-09 14:18:24 +0300

    added port

--- playwright.config.js
+++ playwright.config.js
@@ -74,7 +74,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start-prod',
-    url: 'http://localhost:5001',
+    port: 5001,
     reuseExistingServer: !process.env.CI,
   },
 })
